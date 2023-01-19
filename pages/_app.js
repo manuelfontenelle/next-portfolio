ae8eb57f783182aae1a3ef51bb82e2da43854925
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2023-01-19 21:29:15 +0100

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+// import "../styles/globals.css"
+import "../styles/scss/style.scss"
+import "../styles/globals.css"
+
+function MyApp({ Component, pageProps }) {
+	return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ pages/contact.js
@@ -0,0 +1,8 @@
+// rafce == >  type for create react app snipper
+import React from "react"
+
+const contact = () => {
+	return <div>contact</div>
+}
+
+export default contact
